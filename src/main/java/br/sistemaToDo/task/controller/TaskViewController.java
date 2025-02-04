af30393dc83dbfab5f7f3551fb4d73d5907840af
thaysssss <thaysfernandes.ti@gmail.com>
2025-02-04 10:07:32 -0300

    first commit

--- src/main/java/br/sistemaToDo/task/controller/TaskViewController.java
+++ src/main/java/br/sistemaToDo/task/controller/TaskViewController.java
@@ -5,16 +5,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/tasks")
 public class TaskViewController {
 
-    @Autowired
-    private TaskService taskService;
-
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("tasks", taskService.getAllTasks());
-        return "index";
+    public String listTasks(Model model) {
+        return "tasks/index";
+    }
+
+    @GetMapping("/{id}")
+    public String showTask(@PathVariable Long id, Model model) {
+        return "tasks/show";
     }
 }
