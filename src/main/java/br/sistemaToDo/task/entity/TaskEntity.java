9088adfdeb24474a84d2eed61782555f20cf5d9d
thaysssss <thaysfernandes.ti@gmail.com>
2025-02-04 10:26:08 -0300

    first commit

--- src/main/java/br/sistemaToDo/task/entity/TaskEntity.java
+++ src/main/java/br/sistemaToDo/task/entity/TaskEntity.java
@@ -1,13 +1,11 @@
 package br.sistemaToDo.task.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "task_entity") // Define o nome da tabela
 public class TaskEntity {
 
     @Id
